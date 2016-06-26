f6b8669219be04dc0f5b8da838095bedb2629f03
Jay Milnamow <jaymilnamow@Zipcoder29s-MacBook-Pro.local>
2016-06-25 20:19:04 -0400

    another white board problem down :)

---
+++ src/main/java/RotateArray.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/**
+ * Created by jaymilnamow on 6/25/16.
+ */
+public class RotateArray {
+
+    int[] reorderArray(int[] intArray, int x) {
+
+        int[] reorderedIntArray = new int[intArray.length];
+        for (int j = intArray.length - x; j < intArray.length; j++) {
+            reorderedIntArray[j - x] = intArray[j];
+
+        }
+
+        for (int i = 0; i < intArray.length - x; i++) {
+            reorderedIntArray[i + x] = intArray[i];
+        }
+
+        return reorderedIntArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] intArray = {1, 2, 3, 4};
+        int x = 2;
+
+        RotateArray rotateArray = new RotateArray();
+        int[] reorderedIntArray = rotateArray.reorderArray(intArray,x);
+        System.out.print(Arrays.toString(reorderedIntArray));
+    }
+}
